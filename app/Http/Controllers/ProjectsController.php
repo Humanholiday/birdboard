145387d55260747fbb91c1c23d966b41a620a4b9
Jack Healy <jackhealy85@gmail.com>
2020-08-14 15:26:46 +0100

    Add authentication for users and write test to confirm

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -7,16 +7,32 @@ use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $projects = Project::all();
-
 
         return view('projects.index', compact('projects'));
     }
 
-    public function store(){
+    public  function  show(Project $project)
+    {
+
+        return view('projects.show', compact('project'));
+
+    }
+
+    public function store()
+    {
         //validate
-        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+//        $attributes['owner_id'] = auth()->id();
+
+        auth()->user()->projects()->create($attributes);
+
         //persist
         Project::create($attributes);
 
--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -10,8 +10,18 @@ class ProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
+    public function test_only_authenticated_users_can_create_projects()
+    {
+        $attributes = factory('App\Project')->raw();
+
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
     public function test_a_user_can_create_a_project()
     {
+
+        $this->actingAs(factory('App\User')->create());
+
         $this->withoutExceptionHandling();
 
         $attributes = [
@@ -25,8 +35,21 @@ class ProjectsTest extends TestCase
         $this->get('/projects')->assertSee($attributes['title']);
     }
 
+    public function test_a_user_can_view_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
+
     public function test_a_project_requires_a_title()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['title' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
@@ -34,9 +57,13 @@ class ProjectsTest extends TestCase
     }
     public function test_a_project_requires_a_description()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
+
+
 
 }
